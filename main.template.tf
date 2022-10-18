cf718ec0cbf09e01ff5d27f624d239fc5c127e9a
Dias Saparov <dias@digger.dev>
2022-10-18 12:49:59 +0100

    get rid of variables

--- main.template.tf
+++ main.template.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-  region = "{{ variables.aws_region }}"
+  region = "{{ aws_region }}"
   {% if not for_local_run % } 
     {% if assume_role_arn %}
   assume_role {
-    role_arn="{{variables.assume_role_arn}}"
-    external_id="{{variables.assume_role_external_id}}"
+    role_arn="{{assume_role_arn}}"
+    external_id="{{assume_role_external_id}}"
   }
     {% else %}
-  access_key = "{{variables.aws_access_key}}"
-  secret_key = "{{variables.aws_secret_key}}"
+  access_key = "{{aws_access_key}}"
+  secret_key = "{{aws_secret_key}}"
     {% endif %}  
   {% endif %}
 }
@@ -16,28 +16,28 @@ provider "aws" {
 {% for module in modules %}
     {% if module.type == "vpc" %}
 {{ 
-module "network-{{module.variables.network_name}}" {
+module "network-{{module.network_name}}" {
   source = "{{ module.path_to_module }}"
-  network_name = "{{module.variables.network_name}}"
+  network_name = "{{module.network_name}}"
   tags = {
-    digger_identifier = "{{aws_app_identifier}}"
+    digger_identifier = "{{module.aws_app_identifier}}"
   }
 } 
 }}
      {% elif module.type == "container" %}
 {{ 
 
-module "container-{{module.variables.aws_app_identifier}}" {
+module "container-{{module.aws_app_identifier}}" {
   source = "{{ module.path_to_module }}"
   vpc_id = module.network.vpc_id
-  app = "{{module.variables.aws_app_identifier}}"
-  ecs_cluster_name = "{{module.variables.aws_app_identifier}}"
+  app = "{{module.aws_app_identifier}}"
+  ecs_cluster_name = "{{module.aws_app_identifier}}"
   private_subnets = module.network.private_subnets
   public_subnets = module.network.public_subnets
   monitoring_enabled = false
   alarms_sns_topic_arn = ""
   tags = {
-    digger_identifier = "{{aws_app_identifier}}"
+    digger_identifier = "{{module.aws_app_identifier}}"
 }
 }
 
