afe2354cb8299f0438927eea51b54643856ec86e
Dias Saparov <dias@digger.dev>
2022-10-19 17:05:34 +0100

    fix

---
+++ main.template.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region = "{{ aws_region }}"
+  {% if not for_local_run %} 
+    {% if assume_role_arn %}
+  assume_role {
+    role_arn="{{assume_role_arn}}"
+    external_id="{{assume_role_external_id}}"
+  }
+    {% else %}
+  access_key = "{{aws_access_key}}"
+  secret_key = "{{aws_secret_key}}"
+    {% endif %}  
+  {% endif %}
+}
+
+{% for module in modules %}
+  {% if module.type == "vpc" %}
+module "network" {
+  source = "./{{ module.module_name }}"
+  network_name = "{{module.network_name}}"
+  region = "{{ aws_region }}"
+  tags = {
+    digger_identifier = "{{module.network_name}}"
+  }
+} 
+  {% elif module.type == "container" %}
+module "{{ module.module_name }}" {
+  source = "./{{ module.module_name }}"
+  vpc_id = module.network.vpc_id
+  app = "{{module.aws_app_identifier}}"
+  ecs_cluster_name = "{{module.aws_app_identifier}}"
+  private_subnets = module.network.private_subnets
+  public_subnets = module.network.public_subnets
+  region = "{{ aws_region }}"
+  monitoring_enabled = false
+  alarms_sns_topic_arn = ""
+  tags = {
+    digger_identifier = "{{module.aws_app_identifier}}"
+  }
+}
+  {% elif module.type == "resource" %}
+module "{{ module.module_name }}" {
+  source = "./{{ module.module_name }}"
+  vpc_id = module.network.vpc_id
+  private_subnets = module.network.private_subnets
+  public_subnets = module.network.public_subnets
+  security_groups = flatten({{ security_groups | join(", ") }})
+  aws_app_identifier = "{{module.aws_app_identifier}}"
+  region = "{{ aws_region }}"
+  tags = {
+    digger_identifier = "{{module.aws_app_identifier}}"
+  }
+}
+  {% endif %}
+{% endfor %}
+
+
